18a9240c15d9b4d54cdca6c331f9a3085c02c8cd
todehan <todehan@gmail.com>
2019-02-27 21:01:52 +0300

    errors fixed

--- github.py
+++ github.py
@@ -1,10 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import requests
 
 app = Flask(__name__)
+baseUrl = "https://api.github.com/users/"
 
-@app.route("/")
+@app.route("/",methods = ["GET","POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        githubname = request.form.get("githubname")
+        response = requests.get(baseUrl + githubname)
+        userInfo = response.json()
+        if "message" in userInfo:
+            return render_template("index.html", error = "Kullanici Bulunamadi")
+        else:    
+            return render_template("index.html", profile = userInfo) 
+    else:
+        return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
